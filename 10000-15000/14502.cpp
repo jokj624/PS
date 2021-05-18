9ec29531f132a396ae7422ad50caba0f467ba0c2
Chae Jeong Ah <jokj624@gmail.com>
2021-05-18 17:11:28 +0900

    10000-15000

--- 10000-15000/14502.cpp
+++ 10000-15000/14502.cpp
@@ -13,8 +13,7 @@ int bfs(int bfsmap[][10]){
 	queue<pair<int, int> > q;
 	for(int i=0; i<N; i++){
 		for(int j=0; j<M; j++){
-		//	printf("%d ", bfsmap[i][j]);
-			if(bfsmap[i][j] == 2)	q.push({i, j});
+			if(bfsmap[i][j] == 2)	q.push({i, j});  // 초기 바이러스 위치 저장
 		}
 	}
 	while(!q.empty()){
@@ -24,9 +23,9 @@ int bfs(int bfsmap[][10]){
 		for(int i=0; i<4; i++){
 			int nx = a + dx[i];
 			int ny = b + dy[i];
-			if(nx < 0 || ny < 0 || nx >= N || ny >= M)	continue;
-			if(bfsmap[nx][ny] == 0){
-				bfsmap[nx][ny] = 2;
+			if(nx < 0 || ny < 0 || nx >= N || ny >= M)	continue;   // 경계선 예외 처리
+			if(bfsmap[nx][ny] == 0){    // 해당 위치 0이고 도달 가능하면 바이러스로 바꿔준다.
+				bfsmap[nx][ny] = 2;  
 				q.push({nx, ny});
 			}
 		}
@@ -34,31 +33,31 @@ int bfs(int bfsmap[][10]){
 	int cnt = 0;
 	for(int i=0; i<N; i++){
 		for(int j=0; j<M; j++){
-			if(bfsmap[i][j] == 0)	cnt += 1;
+			if(bfsmap[i][j] == 0)	cnt += 1;   // 바이러스 도달 못한 곳 카운팅
 		}
 	}
-	return cnt;
+	return cnt;  // 해당 탐색에서 결과 반환
 }
 int wallDfs(int wall, int x, int y, int arr[][10]){
-	if(wall == 3){
+	if(wall == 3){    // wall 3개면 바로 bfs로 바이러스 뿌리기 시작한다
 		int tmpmap[10][10];
 		for(int i=0; i<N; i++){
 			for(int j=0; j<M; j++){
-				tmpmap[i][j] = arr[i][j];
+				tmpmap[i][j] = arr[i][j];   // bfsmap 배열에 보낼 copy 배열 생성 
 			}
 		}
-		return bfs(tmpmap);
+		return bfs(tmpmap);   
 	}
-	for(int i = x; i<N; i++){
+	for(int i = x; i<N; i++){   // 가로 행 이전은 모두 이전에 했었으니 x행 부터 시작
 		for(int j=0; j<M; j++){
 			if(arr[i][j] == 0){
-				arr[i][j] = 1;
-				ans = max(ans, wallDfs(wall+1, i, j, arr));
-				arr[i][j] = 0;
+				arr[i][j] = 1;   // 벽 1개 만들고 
+				ans = max(ans, wallDfs(wall+1, i, j, arr));   // 재귀로 벽 1개 더 생성
+				arr[i][j] = 0;   // 벽 원래대로 
 			}
 		}
 	}
-	return ans;
+	return ans;  // 정답 반환
 }
 int main(){
 	int res = 0;
@@ -71,12 +70,12 @@ int main(){
 	for(int i=0; i<N; i++){
 		for(int j=0; j<M; j++){
 			if(map[i][j] == 0){
-				map[i][j] = 1;
-				res = max(res,wallDfs(1, i, j, map));
-				map[i][j] = 0;
+				map[i][j] = 1;  // 첫 벽 1개 생성
+				res = max(res,wallDfs(1, i, j, map));   // 이어서 벽 만들고 반환된 정답 비교
+				map[i][j] = 0;   // 첫번째로 만든 벽 원래대로
 			}
 		}
 	}
-	printf("%d", res);
+	printf("%d", res); 
 	return 0;
 }
