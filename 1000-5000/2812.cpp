ddc6449b3f7c821567ff76fb81f6bed42d689116
jokj624 <jokj624@gmail.com>
2021-06-08 01:43:08 +0900

    1000-5000

--- 1000-5000/2812.cpp
+++ 1000-5000/2812.cpp
@@ -1,48 +1,30 @@
 ﻿//AC
-//BOJ 2812 크게 만들기 
-//https://www.acmicpc.net/problem/2812 
+//BOJ 2812 크게 만들기
 #include <iostream>
 #include <deque>
+#include <string>
 using namespace std;
 int num[500001] = {0};
 deque<int> dq;
 int main(){
 	int n, k, idx;
 	scanf("%d %d", &n, &k);
-	idx = n-k;
+	string str;
+	cin >> str;
 	for(int i=0; i<n; i++){
-		scanf("%1d", &num[i]);
+		num[i] = str[i] - '0';
 	}
 	for(int i=0; i<n; i++){
-		if(dq.empty()){
-			dq.push_back(num[i]);
+		while(!dq.empty() && k && dq.back()<num[i]){
+			dq.pop_back();
+			k--;
 		}
-		else{
-			while(k&&dq.back()<num[i]){
-				dq.pop_back();
-				k--;
-				if(dq.empty())	break;
-			}
-			dq.push_back(num[i]);
-		}
-		if(k==0)	break;
+		dq.push_back(num[i]);
+		
 	}
-	if(dq.size()==n){
-		for(int i=0; i<idx; i++){
-			printf("%d", num[i]);
-		}
-		return 0;
-	}
-	idx-=dq.size();
-	while(!dq.empty()){
-		deque<int>::iterator temp = dq.begin();
-		printf("%d",*temp);
-		dq.pop_front();
-	}
-	if(idx!=0){
-		for(int i=n-idx; i<n; i++){
-			printf("%d", num[i]);
-		}
+
+	for(int i=0; i<dq.size()-k; i++){
+		printf("%d", dq[i]);
 	}
 	return 0;
 }
