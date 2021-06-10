54d13a448a1ebc170f66f571b66d72d261679e6e
jokj624 <jokj624@gmail.com>
2021-06-10 18:28:44 +0900

    10000-15000

---
+++ 10000-15000/10172.cpp
@@ -0,0 +1,11 @@
+//AC
+//BOJ 10172 개
+#include <iostream>
+using namespace std;
+int main(){
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\");
+    printf("\n|\"^\"`    |\n");
+    printf("||_/=\\\\__|");
+}
