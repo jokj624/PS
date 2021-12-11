d52914185a9a3302fa5cc59901b8c2eb85ac1b48
jokj624 <jokj624@gmail.com>
2021-12-11 19:11:33 +0900

    10000-15000

---
+++ 10000-15000/10718.java
@@ -0,0 +1,12 @@
+//AC
+//BOJ 10718 We love kriii
+import java.util.*;
+public class Main{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}
+
+
