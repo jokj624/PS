bd85269f8b2d37334596f1d144120a64f9fc2f3d
jokj624 <jokj624@gmail.com>
2022-03-27 23:33:58 +0900

    10000-15000

---
+++ 10000-15000/14645.cpp
@@ -0,0 +1,13 @@
+//AC
+//BOJ 14645 와이버스 부릉부릉
+#include <iostream>
+using namespace std;
+int main(){
+  int N, M;
+  cin >> N >> M;
+  for(int i=0; i<N; i++){
+    int a,b;
+    cin >> a >> b;
+  }
+  cout << "비와이";
+}
