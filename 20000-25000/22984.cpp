d310ba581cb2d734b588dbb4d7d26fb094fba3d2
jokj624 <jokj624@gmail.com>
2022-04-25 21:42:03 +0900

    20000-25000

---
+++ 20000-25000/22984.cpp
@@ -0,0 +1,23 @@
+// AC
+// BOJ 22984 반짝반짝 2
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string>
+using namespace std;
+typedef double ld;
+ld p[101010];
+int main() {
+    int N;
+    ld ans = 0;
+    cin >> N;
+    for(int i=0; i<N; i++) {
+        cin >> p[i];
+        ans += p[i];
+    }
+    for(int i=0; i<N-1; i++) {
+        ans += (p[i]*(1-p[i+1]) + p[i+1]*(1-p[i]));
+    }
+    printf("%.9lf", ans);
+}
